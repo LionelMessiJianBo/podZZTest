c0619586cf2d7ef145811f2dc0d14422f325af81
bianjianbo <bianjianbo@fang.com>
2020-10-29 21:31:34 +0800

    1.0.0

---
+++ Tool/WebSaveManager1.h
@@ -0,0 +1,9 @@
+//
+//  WebSaveManager1.h
+//  SouFun
+//
+//  Created by fcs on 2020/2/8.
+//  Copyright © 2020 房天下 Fang.com. All rights reserved.
+//
+
+
---
+++ Tool/WebSaveManager1.m
@@ -0,0 +1,8 @@
+//
+//  WebSaveManager1.m
+//  SouFun
+//
+//  Created by fcs on 2020/2/8.
+//  Copyright © 2020 房天下 Fang.com. All rights reserved.
+//
+
